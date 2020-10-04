5f19572779b979c8305d5b3f61754e17a32c3c4d
zelr0x <zelr0xdev@gmail.com>
2020-10-04 21:32:58 +0400

    Fixed solution for 1.16

--- ch1/e1_16.scm
+++ ch1/e1_16.scm
@@ -5,8 +5,9 @@
 (define (fast-expt b n)
   (define (iter b n a)
     (cond ((= n 0) a)
-          (even? n)) (iter (square b) (/ n 2) a))
-          (else (iter b (- n 1) (* b a))))
+          ((even? n) (iter (square b) (/ n 2) a))
+          (else (iter b (- n 1) (* b a)))))
+  (iter b n 1))
 
 (display (fast-expt 2 10))
 
