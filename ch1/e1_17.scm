87705b4b9de6be422c7f90ed4e522b5ff911cd0c
zelr0x <zelr0xdev@gmail.com>
2020-10-04 23:14:42 +0400

    Fixed 1.17 implementation since it was incorrect for some cases

--- ch1/e1_17.scm
+++ ch1/e1_17.scm
@@ -2,10 +2,11 @@
 (define (halve n) (/ n 2))
 
 (define (* a b)
-  (cond ((< b 0) a)
-        ((= b 0) 0)
-        ((not (< b 2)) (* (double a) (halve b)))
-        (else (* (+ a a) (- b 1)))))
+  (define (iter a b acc)
+    (cond ((= b 0) acc)
+          ((even? b) (iter (double a) (halve b) acc))
+          (else (iter a (- b 1) (+ acc a)))))
+  (iter a b 0))
 
 (display (* 5 10))
 
