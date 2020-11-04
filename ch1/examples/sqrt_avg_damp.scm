b0cb838e355910171214cd1210aabe6172da8ee7
zelr0x <zelr0xdev@gmail.com>
2020-11-04 23:54:04 +0400

    Added examples

---
+++ ch1/examples/sqrt_avg_damp.scm
@@ -0,0 +1,23 @@
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (average a b) (/ (+ a b) 2))
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+(define (sqrt x)
+  (fixed-point (average-damp (lambda (y) (/ x y)))
+               1.0))
+
+(define (cube-root x)
+  (fixed-point (average-damp (lambda (y) (/ x (square y))))
+               1.0))
+
