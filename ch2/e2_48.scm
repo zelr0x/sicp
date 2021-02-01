3164a9c7a4bc3111d67a3e18938b65913a637dde
zelr0x <zelr0xdev@gmail.com>
2021-02-01 22:47:06 +0400

    Added solution to 2.48

---
+++ ch2/e2_48.scm
@@ -0,0 +1,21 @@
+#lang sicp
+
+(define (make-vect x y) (cons x y))
+(define (xcor-vect v) (car v))
+(define (ycor-vect v) (cdr v))
+(define (add-vect a b)
+  (make-vect (+ (xcor-vect a) (xcor-vect b))
+             (+ (ycor-vect a) (ycor-vect b))))
+(define (sub-vect a b)
+  (make-vect (- (xcor-vect a) (xcor-vect b))
+             (- (ycor-vect a) (ycor-vect b))))
+(define (scale-vect v s)
+  (make-vect (* (xcor-vect v) s)
+             (* (ycor-vect v) s)))
+
+(define origin 0)
+(define (make-segment start end)
+  (cons (make-vect origin start)
+        (make-vect origin end)))
+(define (start-segment s) (car s))
+(define (end-segment s) (cdr s))
