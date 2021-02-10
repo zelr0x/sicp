0b77faac36e300385af336d5dd7935c08702562e
zelr0x <zelr0xdev@gmail.com>
2021-02-10 23:58:54 +0400

    Added solution to 2.61

---
+++ ch2/e2_61.scm
@@ -0,0 +1,34 @@
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (cond ((or (null? set) (< x (car set))) (cons x set))
+        ((= x (car set)) set)
+        (else (cons (car set) (adjoin-set x (cdr set))))))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+    '()
+    (let ((x1 (car set1)) (x2 (car set2)))
+      (cond ((= x1 x2)
+             (cons x1 (intersection-set (cdr set1)
+                                        (cdr set2))))
+            ((< x1 x2)
+             (intersection-set (cdr set1) set2))
+            ((< x2 x1)
+             (intersection-set set1 (cdr set2)))))))
+
+(define (union-set set1 set2)
+  (accumulate adjoin-set set1 set2))
+
+(display (union-set '(1 2 3) '(1 2 5 6)))
+
